32e05222a63dfbc08d8956f1edeba730ce11b83b
Youngseo Lee <seoya.lee512@gmail.com>
2022-10-15 13:46:28 +0900

    Updated JS 100 ver2 #54,56

--- 01_코딩테스트연습/javascript/JS100제/Code100_2.js
+++ 01_코딩테스트연습/javascript/JS100제/Code100_2.js
@@ -43,3 +43,57 @@ function mathBrackets(brackets){
   }else{
     console.log("NO");
   }
+
+  //문제 54: 연속되는 수
+
+  function check(num){
+    // 스탬프 숫자가 오름차순 정렬
+    num.sort((a,b) => a-b)
+    
+    // 연속적인 숫자인지 확인 
+    for(let i=0; i< num.length -1; i++){
+      if (num[i]+1 !== num[i+1]){
+        return "NO"
+      }
+    }
+     return "YES"
+  }
+  
+  // 스탬프 숫자 입력
+  const n = prompt('입력해주세요').split(' ').map(n => parseInt(n, 10));
+
+  console.log(check(n));
+  
+
+  // 문제 56: 객체의 험수 응용 (풀이)
+  const nationWidth = {
+         'korea': 220877,
+         'Rusia': 17098242,
+         'China': 9596961,
+         'France': 543965,
+         'Japan': 377915,
+         'England' : 242900,
+    }
+    
+    // 고정값 정해주고, 필요 없는 속성 지움
+    const fixed = nationWidth['korea'];
+    delete nationWidth['korea']; 
+    
+    // Object.entries(): 객체를 Array로 전달함으로, 접근을 조금 더 용이하게 만들어줌 
+    const entry = Object.entries(nationWidth); 
+    // Object.values(): 객체의 값만 뽑아 Array로 전달
+    const values = Object.values(nationWidth); 
+    
+    // 최댓값 설정 (조건문을 위해 임의로 설정)
+    let gap = Math.max.apply(null, values);
+    let item = [];
+    
+    // 값 비교를 통해 결국 최소값을 찾게됨
+    for(let i in entry){
+      if(gap > Math.abs(entry[i][1] - fixed)){
+        gap = Math.abs(entry[i][1]- fixed);
+        item = entry[i]
+      }
+    }
+    
+    console.log(item[0], item[1]- fixed);
